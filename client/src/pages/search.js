f258e40ecde77964913d6b835ce969803c6798e4
Glen D Fabert <glen.fabert@gmail.com>
2020-12-06 20:33:17 -0500

    added button for search

--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import API from "../utils/API";
-import { Input } from "../components/form";
+import { Input, FormBtn } from "../components/form";
 
 function Search() {
+  const [books, setBooks] = useState([])
+  const [formObject, setFormObject] = useState({})
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    };
  
 
   return (
     <div>
         Search Page
         <form>
-            <Input
-              name='title'
-              placeholder='enter title'
-            />
-        </form>
+              <Input
+                name="title"
+                placeholder="Title"
+              />
+              <FormBtn
+                onClick={handleFormSubmit}
+              >
+                Submit Book
+              </FormBtn>
+            </form>
     </div>    
   );
 }
